fee0e7d9bd297d2e2ee45571267fdb6e1b13e3bc
Jeffrey Yu <35394596+JeffreytheCoder@users.noreply.github.com>
2024-07-29 19:45:02 -0700

    Update demo page EC to server with LLM

--- demo/src/views/EmbeddedChat/EmbeddedChat.js
+++ demo/src/views/EmbeddedChat/EmbeddedChat.js
@@ -1,21 +1,21 @@
-import { EmbeddedChat } from "ec-test";
-import StormySeas from "../../theme/StormySeas";
-import styles from "./EmbeddedChat.module.css";
+import { EmbeddedChat } from 'ec-test';
+import StormySeas from '../../theme/StormySeas';
+import styles from './EmbeddedChat.module.css';
 
 const ECComponent = ({ isEcOpen, setIsECOpen }) => {
   return (
     <div className={styles.ecContainer}>
       <EmbeddedChat
-        host="https://spiral-memory.rocket.chat"
-        roomId="6679b390ef5048f098b06d48"
+        host="https://demo.qa.rocket.chat"
+        roomId="669fd7388129fb4346981692"
         anonymousMode
         theme={StormySeas}
         height="85vh"
         width="55vw"
         auth={{
-          flow: "TOKEN",
+          flow: 'TOKEN',
           credentials: {
-            resume: "czWwA8NzkBkch7lzl3G-yc6Pm5Gqco9PBf-mu4CyLqk",
+            resume: 'RXINgch72chNT_3WRyXWeUJXR9phnUQdP5RVyVvicvd',
           },
         }}
         isClosable={true}
